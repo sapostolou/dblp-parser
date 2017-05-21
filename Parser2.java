e2dd5bc5570deb0acd7aac850051025b717f2b27
spiros <s.st.apostolou@gmail.com>
2017-05-21 14:43:47 +0300

    Refactoring and formatting changes

--- Parser2.java
+++ Parser2.java
@@ -23,6 +23,7 @@ public class Parser2 {
 
 			SAXParserFactory factory = SAXParserFactory.newInstance();
 			SAXParser saxParser = factory.newSAXParser();
+
             int maxYear = Integer.parseInt(args[0]);
 			UserHandler userhandler = new UserHandler(maxYear);
 			saxParser.parse(inputFile, userhandler);
@@ -44,7 +45,9 @@ public class Parser2 {
 			} catch (Exception e){
 				e.printStackTrace();
 			}
+
 			PersonCollection personCollection = userhandler.getPersonCollection();
+			
 			for (Person p : personCollection.getCollection()){
 				idAndName.println(p.getID() + "\t" + p.getName());
 				idAndConf.println(p.getID() + "\t" + p.getMostCommonConf());
@@ -81,20 +84,15 @@ public class Parser2 {
 
 class UserHandler extends DefaultHandler {
 	int MAX_YEAR;
-	boolean bFirstName = false;
-	boolean bLastName = false;
-	boolean bNickName = false;
-	boolean bMarks = false;
 	boolean insideConf = false;
-	int count =0;
-	String Value;
+	int inproceedingsCount = 0;
+	String content;
 	String year;
 	boolean insidePerson;
 	HashMap<String,Integer> nameToID = new HashMap<String,Integer>();
 	int maxID = 0;
 	ArrayList<Integer> persons;
-	String key, recordTag;
-	int yearOfPub;
+	String key, elementName;
  
 	PersonCollection personCollection = new PersonCollection();
 	String pubType, confName;
@@ -108,7 +106,7 @@ class UserHandler extends DefaultHandler {
 
 
 	public UserHandler(int maxYear){
-		MAX_YEAR=maxYear;
+		MAX_YEAR = maxYear;
 		try{
 			//PrintWriter conferencesList = new PrintWriter("conferencesList.txt","UTF-8");
 			BufferedReader br = new BufferedReader(new FileReader("conferencesList.txt"));
@@ -155,46 +153,41 @@ class UserHandler extends DefaultHandler {
 	}
 
 	@Override
-	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-		// START OF AUTHOR OR EDITOR ELEMENT
-		String k;
-		if (insidePerson = (qName.equals("author") || qName.equals("editor"))) {
-			Value = "";
+	public void startElement(String uri, String localName, String eName, Attributes attributes) throws SAXException {
+		elementName = eName;
+		String k = attributes.getcontent("key");
+		if (insidePerson = (elementName.equals("author") || elementName.equals("editor"))) {
+			content = "";
 			return;
 		}
 
 		// START OF INPROCEEDINGS ELEMENT
-		if((attributes.getLength()>0) && (k = attributes.getValue("key"))!=null){
-			year = attributes.getValue("mdate").split("-")[0];
-			/*if(Integer.parseInt(year)<2011){
-				return;
-			}*/
-			//String k = attributes.getValue("key");
+		if((attributes.getLength()>0) && k != null){
+			year = Integer.parseInt(attributes.getcontent("mdate").split("-")[0]);
 			pubType = k.split("/")[0];
 			confName = k.split("/")[1];
-			if( Integer.parseInt(year)<MAX_YEAR || !conferences.contains(confName)) {
+
+			if( year < MAX_YEAR || !conferences.contains(confName)) {
 				return;
 			}
-			yearOfPub = Integer.parseInt(year);
-			if(pubType.equals("conf") && qName.equals("inproceedings")){
+
+			if(pubType.equals("conf") && elementName.equals("inproceedings")){
 				foundInproceedings=true;
 				insideConf = true;
-				/*key = confName;*/
-				recordTag = qName;
-				//conferences.add(confName);
+				
 				persons = new ArrayList<Integer>();
 			}
     	}
     }
 
     @Override
-    public void endElement(String uri, String localName, String qName) throws SAXException {
-    	if ((qName.equals("author") || qName.equals("editor")) && insideConf){	// author or editor tag inside inproceedings tag
+    public void endElement(String uri, String localName, String eName) throws SAXException {
+    	if ((eName.equals("author") || eName.equals("editor")) && insideConf){	// author or editor tag inside inproceedings tag
 			String conferenceName = confName;
 			Person p;
-			if ((p = PersonCollection.getPersonIfExists(Value)) == null){
+			if ((p = PersonCollection.getPersonIfExists(content)) == null){
 				// person doesn't exist already in collection
-				p = PersonCollection.putPersonInCollection(Value);
+				p = PersonCollection.putPersonInCollection(content);
 			}
 			p.addConference(conferenceName);
             p.addField(confToField.get(conferenceName));
@@ -202,38 +195,18 @@ class UserHandler extends DefaultHandler {
 			persons.add(p.getID());
 			insidePerson=false;
     	}	
-    	else if (qName.equals(recordTag) && insideConf){		// closing element is inproceedings
-    		count = count +1;
-    		System.out.print(count + "\r");
-    		// number of inproceedings records is 1252573
+    	else if (eName.equals(elementName) && insideConf){		// closing element is inproceedings
+    		inproceedingsCount = inproceedingsCount + 1;
+    		System.out.print(inproceedingsCount + "\r");
     		
     		// persons is an arraylist of all ids of persons in the currently closing inproceedings record.
 
     		if (persons.isEmpty()){
     			return;
     		}
-    		/*for (Integer i : persons){
-    			if (IDtoConfCount.containsKey(one)){
-    				personMap = IDtoConfCount.getValue(one);
-    				if(personMap.containsKey(recordTag)){
-    					personMap.put(recordTag,personMap.get(recordTag)+1);
-    				}
-    				else{
-    					personMap.put(recordTag,1);
-    				}
-    			}
-    			else{
-    				tempMap = new HashMap<String,Integer>();
-    				tempMap.put(recordTag,1);
-    				IDtoConfCount.put(one,tempMap);
-    			}
-
-    		}*/
-    		// if(yearOfPub>=2015){
-    		// 	printEdges(persons);
-    		// }
-    		// yearOfPub=0;
+			
     		printEdges(persons);
+
     		insideConf = false;
     	}
     }
@@ -241,7 +214,7 @@ class UserHandler extends DefaultHandler {
     @Override
     public void characters(char ch[], int start, int length) throws SAXException {
     	if (insidePerson)
-    		Value += new String(ch, start, length);
+    		content += new String(ch, start, length);
     }
 
     void printEdges(ArrayList<Integer> list){
